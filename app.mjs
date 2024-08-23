ce207c38d3554fac75d98850036edc4b18e83150
Prajna <prajna.yogish4@gmail.com>
2024-08-23 11:50:18 -0400

    created LatestNews

--- app.mjs
+++ app.mjs
@@ -1,55 +1,56 @@
-const API_KEY =  "pub_51547876bdf2e18cfc7dc5ce01b9cbe93b59d";
+import { getLatestNews } from "./LatestNews.mjs";
+
+
 let outerContainer = document.querySelector('.news-container')
 
 
-
-
-async function getLatestNews(){
-     let response = await fetch('https://newsdata.io/api/1/latest?apikey=pub_51547876bdf2e18cfc7dc5ce01b9cbe93b59d&country=in&language=en');
-    let jsonData = await response.json();
-
-    console.log(jsonData);
-    console.log(jsonData.results[0].url)
-
+function displayNews(jsonData){
     jsonData.results.forEach((item)=>{
 
-let oneContainer = document.createElement('div')
-oneContainer.classList.add('box-style')
-
-let newsImage = document.createElement('img');
-newsImage.classList.add('news-img')
-newsImage.setAttribute('src',`${item.image_url}`);
-
-let title = document.createElement('p');
-title.classList.add('title-style')
-title.textContent = `${item.title}`
-
-let bottomDiv = document.createElement('div');
-bottomDiv.classList.add('style-bottom')
-let left = document.createElement('p');
-left.textContent=`${item.source_name}`;
-
-let right = document.createElement('a');
-right.textContent = "see more"
-right.setAttribute('href',`${item.source_url}`);
-
-bottomDiv.appendChild(left)
-bottomDiv.appendChild(right)
-
-oneContainer.appendChild(newsImage)
-oneContainer.appendChild(title);
-oneContainer.appendChild(bottomDiv);
-outerContainer.appendChild(oneContainer);
-
-
-    })
+        let oneContainer = document.createElement('div')
+        oneContainer.classList.add('box-style')
+        
+        let newsImage = document.createElement('img');
+        newsImage.classList.add('news-img')
+        newsImage.setAttribute('src',`${item.image_url}`);
+        
+        let title = document.createElement('p');
+        title.classList.add('title-style')
+        title.textContent = `${item.title}`
+        
+        let bottomDiv = document.createElement('div');
+        bottomDiv.classList.add('style-bottom')
+        let left = document.createElement('p');
+        left.textContent=`${item.source_name}`;
+        
+        let right = document.createElement('a');
+        right.textContent = "see more"
+        right.setAttribute('href',`${item.source_url}`);
+        
+        bottomDiv.appendChild(left)
+        bottomDiv.appendChild(right)
+        
+        oneContainer.appendChild(newsImage)
+        oneContainer.appendChild(title);
+        oneContainer.appendChild(bottomDiv);
+        outerContainer.appendChild(oneContainer);
+           
+            })
+            
+}
     
+    function getAllNews(){
+            getLatestNews().then((data)=>{
+                       displayNews(data);
+                 })
+  
     }
+
+
+
     
     
-    
-    
-    getLatestNews();
+    getAllNews();
 
 
 
---
+++ latestNews.mjs
@@ -0,0 +1,15 @@
+export {getLatestNews};
+
+const API_KEY =  "pub_51547876bdf2e18cfc7dc5ce01b9cbe93b59d";
+async function getLatestNews(){
+    //  let response = await fetch('https://newsdata.io/api/1/latest?apikey=pub_51547876bdf2e18cfc7dc5ce01b9cbe93b59d&country=in&language=en');
+    let jsonData = await response.json();
+return jsonData;
+    // console.log(jsonData);
+    // console.log(jsonData.results[0].url)
+
+    
+    }
+    
+
+
